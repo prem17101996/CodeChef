042f8a0750344cd69ca992e363dd8ff6374afe44
Prem <premmishra17101996@gmail.com>
2021-03-29 22:06:49 +0530

    Added a program two vs ten

---
+++ Two vs Ten.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    x = int(input())
+    if (x % 10 == 0):
+        print("0")
+    elif ((x % 5 == 0)&(x%10!=0)):
+        print("1")
+
+    else:
+        print("-1")
